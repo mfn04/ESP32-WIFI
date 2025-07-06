7d4133a9f5bfa6e6be77a5bad269fc2afeab9873
Arcadius <mfn.45@outlook.com>
2025-07-06 14:16:39 +0300

    created error handler and connect to network functions

--- src/main.c
+++ src/main.c
@@ -4,9 +4,7 @@
 #include "stdbool.h"
 #include "string.h"
 
-static bool ssid_found(const char* ssid) {
-
-    esp_err_t err = esp_wifi_scan_start(NULL, true);
+static bool err_handle(esp_err_t err){
     switch(err){
         case ESP_ERR_WIFI_NOT_INIT:
             ESP_LOGW("WIFI ERROR", "WiFi was not initialized!");
@@ -15,14 +13,37 @@ static bool ssid_found(const char* ssid) {
             ESP_LOGW("WIFI ERROR", "WiFi was not started!");
             return false;
         case ESP_ERR_WIFI_STATE:
-            ESP_LOGW("WIFI ERROR", "WiFi can't scan while connecting!");
+            ESP_LOGW("WIFI ERROR", "WiFi is busy connecting!");
             return false;
         case ESP_ERR_WIFI_TIMEOUT:
-            ESP_LOGW("WIFI ERROR", "WiFi scan was blocked by timeout!");
+            ESP_LOGW("WIFI ERROR", "WiFi operation blocked by timeout!");
             return false;
+        case ESP_ERR_WIFI_SSID:
+            ESP_LOGW("WIFI ERROR", "WiFi operation blocked due to invalid SSID!");
+            return false;
+        case ESP_ERR_WIFI_PASSWORD:
+            ESP_LOGW("WIFI ERROR", "WiFi operation blocked due to invalid password!");
+            return false;
+        case ESP_ERR_WIFI_IF:
+            ESP_LOGW("WIFI ERROR", "WiFi operation failed due to interface error!");
+            return false;
+        case ESP_ERR_WIFI_CONN:
+            ESP_LOGW("WIFI ERROR", "WiFi operation failed due to control block of station or soft-AP error!");
+            return false;
+        case ESP_OK:
+            ESP_LOGW("WIFI ERROR", "WiFi operation was successful!");
+            return true;
         default:
-            ESP_LOGI("INFO", "WiFi scan was successful!");
+            ESP_LOGW("WARN", "WiFi operation failed due to unhandled error '%d'!", err);
+            return false;
     }
+}
+
+static bool ssid_found(const char* ssid) {
+
+    esp_err_t err = esp_wifi_scan_start(NULL, true);
+    
+    if(!err_handle(err)) return false;
 
     uint16_t num = 0;
 
@@ -46,6 +67,23 @@ static bool ssid_found(const char* ssid) {
     return found;
 }
 
+static bool connect_to_network(const char* ssid, const char* password) {
+    wifi_interface_t interface = WIFI_IF_STA;
+    wifi_config_t conf = {};
+    strncpy((char*)conf.sta.ssid,ssid,sizeof(conf.sta.ssid));
+    strncpy((char*)conf.sta.password,password,sizeof(conf.sta.password));
+
+    ESP_ERROR_CHECK(esp_wifi_set_config(interface, &conf));
+
+    esp_err_t err = esp_wifi_connect();
+
+    if(!err_handle(err)) return false;
+
+    ESP_LOGI("INFO", "Successfully connected to the WiFi network '%s'!", ssid);
+    return true;
+
+}
+
 
 void app_main() {
 
@@ -63,15 +101,15 @@ void app_main() {
 
     esp_wifi_start();
 
-    const char* ssid = "TP-Link 555 BH";
+    const char* ssid = "";
 
     bool found = ssid_found(ssid);
 
     if(found){
         ESP_LOGI("INFO","Successfully found the WiFi AP '%s'!", ssid);
+        connect_to_network("","");
     } else {
         ESP_LOGI("INFO","WiFi AP '%s' was not found!", ssid);
     }
-
 
 }
